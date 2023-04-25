5f40d4958e53f404d696fd1a48f97d8ec3f556f2
pcasotti <pedro.c.casotti@gmail.com>
2023-04-25 11:26:29 -0300

    Adiciona bot feliz no app

--- app.py
+++ app.py
@@ -2,9 +2,10 @@
 from SistemaChatBot import SistemaChatBot as scb
 from Bots.BotZangado import BotZangado
 from Bots.BotDramatico import BotDramatico
+from Bots.BotFeliz import BotFeliz
 
 ###construa a lista de bots disponíveis aqui
-lista_bots = [BotZangado("Roger"),BotDramatico("Josemildo")]
+lista_bots = [BotZangado("Roger"), BotDramatico("Josemildo"), BotFeliz("Carlos")]
 
 sys = scb.SistemaChatBot("BoterSystem",lista_bots)
 sys.inicio()
