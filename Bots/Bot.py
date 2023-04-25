5b2bce94f71bc7d02df6b55e8ebadc9c7bef90e2
pcasotti <pedro.c.casotti@gmail.com>
2023-04-25 10:15:25 -0300

    Completa classe Bot abstrata

--- Bots/Bot.py
+++ Bots/Bot.py
@@ -5,29 +5,38 @@ import random as r
 
 class Bot(ABC):
 
-    def __init__(nome,):
-        self.nome = nome
-        self.comandos = {}
+    def __init__(self, nome):
+        self._nome = nome
+        self._comandos = {}
 
-    #nao esquecer o decorator
+    @property
     def nome(self):
-        pass
+        return self._nome
 
-    #nao esquecer o decorator
-    def nome(nome):
-        pass
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
 
     def mostra_comandos(self):
-        pass
+        s = ''
+        for i, key in enumerate(sorted(self._comandos.keys())):
+            s += f'{i+1} - {key}\n'
+
+        return s
 
     @abstractmethod
+    def apresentacao(self):
+        pass
+
     def executa_comando(self,cmd):
-        pass
+        if cmd <= 0 or cmd > len(a):
+            return self.despedida()
+        return sorted(self._commandos.keys())[cmd-1]
 
     @abstractmethod
-    def boas_vindas():
+    def boas_vindas(self):
         pass
     
     @abstractmethod
-    def despedida():
+    def despedida(self):
         pass
